b15ebb72ba1254c32076fba7fb1ff5f37eca80bd
dumontjo <joshua.dumont@edu.esiee.fr>
2023-10-18 11:36:52 +0200

    Ajout de l'histogramme

--- main.py
+++ main.py
@@ -1,6 +1,78 @@
+# dash-01.py
+
+#
+# Imports
+#
+
+# Importation des bibliothèques nécessaires pour la visualisation et la création du dashboard
+import plotly.express as px
+import dash
+from dash import dcc, html 
 import pandas as pd
-import numpy as np
 
-print("Bonjour Monsieur ;) bonne correction")
 
-if __name__ == "__main__":
+#
+# Data
+#
+
+# Chargement des données à partir du fichier CSV
+natural_disaster_df = pd.read_csv('natural_disaster.csv')
+
+#
+# Main
+#
+
+# Ce bloc garantit que le code ci-dessous n'est exécuté que lorsque ce script est lancé directement
+if __name__ == '__main__':
+
+    # Initialisation de l'application Dash
+    app = dash.Dash(__name__)
+
+    # Création du premier histogramme en utilisant plotly express
+    fig1 = px.histogram(natural_disaster_df, x="Total Deaths",
+                       title="Distribution du nombre total de décès dus aux catastrophes naturelles",
+                       nbins=50,  # Utilisation de 50 intervalles pour l'histogramme
+                       log_y=True
+                       )
+
+    # Filtrage des données pour inclure uniquement les décès entre 0 et 100,000
+    filtered_data = natural_disaster_df[(natural_disaster_df["Total Deaths"] > 0) & 
+                                        (natural_disaster_df["Total Deaths"] <= 100000)]
+
+    # Création du second histogramme avec les données filtrées
+    fig2 = px.histogram(filtered_data, x="Total Deaths",
+                       title="Distribution des décès (0 à 100,000) dus aux catastrophes naturelles",
+                       nbins=50,
+                       log_y=True)
+
+    # Structure de la mise en page du tableau de bord
+    app.layout = html.Div(children=[
+        # Ajout d'un titre au tableau de bord pour le premier histogramme
+        html.H1(children='Distribution des décès dus aux catastrophes naturelles',
+                style={'textAlign': 'center', 'color': '#7FDBFF'}),
+
+        # Intégration du premier histogramme dans le dashboard
+        dcc.Graph(
+            id='graph1',
+            figure=fig1
+        ),
+
+        # Ajout d'un titre pour le second histogramme
+        html.H1(children='Distribution des décès (0 à 100,000) dus aux catastrophes naturelles',
+                style={'textAlign': 'center', 'color': '#7FDBFF'}),
+
+        # Intégration du second histogramme dans le dashboard
+        dcc.Graph(
+            id='graph2',
+            figure=fig2
+        ),
+
+        # Description textuelle pour fournir des informations complémentaires sur l'histogramme
+        html.Div(children=f'''
+            Les histogrammes représentent la distribution du nombre total de décès 
+            dus aux catastrophes naturelles.
+        '''),
+    ])
+
+    # Démarrage du serveur Dash
+    app.run_server(debug=True)
