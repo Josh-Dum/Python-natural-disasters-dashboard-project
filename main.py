d367a5d3d80cac25762095605d8dbbf8acec7c7d
dumontjo <joshua.dumont@edu.esiee.fr>
2023-10-25 09:32:47 +0200

    Ajout histo 2d

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ app.layout = html.Div(children=[
     # Histogrammes
     dcc.Graph(id='graph1'),
     dcc.Graph(id='graph2'),
+    dcc.Graph(id='graph3'),
+    dcc.Graph(id='graph4'),  # Graphique 2D ajouté
     
     # Description
     html.Div(children=f'''
@@ -41,7 +43,9 @@ app.layout = html.Div(children=[
 # Rappel pour mettre à jour les histogrammes en fonction de la sélection de l'année
 @app.callback(
     [Output('graph1', 'figure'),
-     Output('graph2', 'figure')],
+     Output('graph2', 'figure'),
+     Output('graph3', 'figure'),
+     Output('graph4', 'figure')],  # Graphique 2D ajouté
     [Input('year-slider', 'value')]
 )
 def update_histogram(selected_years):
@@ -49,13 +53,18 @@ def update_histogram(selected_years):
                                       (natural_disaster_df['Year'] <= selected_years[1])]
     
     fig1 = px.histogram(filtered_df, x="Total Deaths",
-                        color="Disaster Subgroup",
                         title="Histogramme global du nombre total de décès dus aux catastrophes naturelles",
                         nbins=50,
-                        log_y=True,
-                        hover_data=["Disaster Subgroup"])
+                        log_y=True)
     
     fig2 = px.histogram(filtered_df[(filtered_df["Total Deaths"] >= 0) & 
+                                    (filtered_df["Total Deaths"] <= 10000)],
+                        x="Total Deaths",
+                        title="Histogramme des décès (0 à 10,000) dus aux catastrophes naturelles",
+                        nbins=100,
+                        log_y=True)
+    
+    fig3 = px.histogram(filtered_df[(filtered_df["Total Deaths"] >= 0) & 
                                     (filtered_df["Total Deaths"] <= 10000)],
                         x="Total Deaths",
                         color="Disaster Subgroup",
@@ -64,7 +73,22 @@ def update_histogram(selected_years):
                         log_y=True,
                         hover_data=["Disaster Subgroup"])
     
-    return fig1, fig2
+    # Histogramme 2D pour "Total Deaths" et "Total Damages"
+    fig4 = px.density_heatmap(filtered_df, 
+                          x="Total Deaths", 
+                          y="Total Damages ('000 US$)",
+                          marginal_x="histogram", 
+                          marginal_y="histogram",
+                          title="Densité de la distribution des décès et des dommages",
+                          range_x=[0, 500],  # Limites pour l'axe x
+                          range_y=[0, 1000000],  # Limites pour l'axe y
+                          nbinsx=100,  # Utilisation de num_bins pour l'axe x
+                          nbinsy=100   # Utilisation de num_bins pour l'axe y
+                         )
+
+
+    
+    return fig1, fig2, fig3, fig4
 
 # Démarrage du serveur Dash
 if __name__ == '__main__':
